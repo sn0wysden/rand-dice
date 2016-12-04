5dddee55fe49cffbca442347d64334a12302673d
Ty <snowyhudjera@gmail.com>
2016-12-03 23:06:07 -0600

    Add files via upload

---
+++ dice.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <stdio.h> 
+#include <stdlib.h>
+#include <time.h> 
+
+int main () {
+	
+	
+	int roll;
+
+	//Get Seed
+	srand (time(NULL));
+	
+	// Pick random number.
+
+	roll = rand() % 6 + 1;
+
+	// Prints ascii die depending on the number above.
+
+	if (roll == 1) {
+
+		std::cout << R"(
+        	+---------+
+        	|         |
+        	|         |
+        	|    .    |
+        	|         |
+        	|         |
+        	+---------+
+        )" ;
+			
+	}
+
+	if (roll == 2) {
+
+		std::cout << R"(
+        	+---------+
+        	|         |
+        	|  .      |
+        	|         |
+        	|      .  |
+        	|         |
+        	+---------+
+        )" ;
+			
+	}
+
+	if (roll == 3) {
+
+		std::cout << R"(
+        	+---------+
+        	|         |
+        	|  .      |
+        	|    .    |
+        	|      .  |
+        	|         |
+        	+---------+
+        )" ;
+			
+	}
+
+	if (roll == 4) {
+
+		std::cout << R"(
+        	+---------+
+        	|         |
+        	|  .   .  |
+        	|         |
+        	|  .   .  |
+        	|         |
+        	+---------+
+        )" ;
+			
+	}
+
+	if (roll == 5) {
+
+		std::cout << R"(
+        	+---------+
+        	|         |
+        	|  .   .  |
+        	|    .    |
+        	|  .   .  |
+        	|         |
+        	+---------+
+        )" ;
+			
+	}
+
+	if (roll == 6) {
+
+		std::cout << R"(
+        	+---------+
+        	|         |
+        	| .  .  . |
+        	|         |
+        	| .  .  . |
+        	|         |
+        	+---------+
+        )" ;
+			
+	}
+}
